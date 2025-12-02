036b8852a18dad7587c52477b2b6db62025a2b8a
Muksit <muksit09876543@gmail.com>
2025-12-03 02:10:06 +0600

    Add Neon Number check program in C
    Implement a program to check if a number is a Neon Number.

---
+++ 35.	C Program to check whether the input number is a Neon Number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, sq, sum = 0;
+    scanf("%d", &n);
+
+    sq = n * n;
+
+    while (sq > 0) {
+        sum += sq % 10;
+        sq /= 10;
+    }
+
+    if (sum == n)
+        printf("Neon Number");
+    else
+        printf("Not a Neon Number");
+
+    return 0;
+}
