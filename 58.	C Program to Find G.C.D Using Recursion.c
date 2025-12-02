6afc37206dd5861c38b107220d37ece4a378ae3a
Muksit <muksit09876543@gmail.com>
2025-12-02 21:55:57 +0600

    Add C program to find G.C.D using recursion

---
+++ 58.	C Program to Find G.C.D Using Recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if(b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("GCD = %d", gcd(x, y));
+    return 0;
+}
