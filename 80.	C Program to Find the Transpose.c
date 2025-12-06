1a8631b4445459f19cc47c20241f37be216aca59
Muksit <muksit09876543@gmail.com>
2025-12-06 13:22:17 +0600

    Create 80.	C Program to Find the Transpose.c

---
+++ 80.	C Program to Find the Transpose.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int r,c,a[50][50],i,j;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<c;i++){
+        for(j=0;j<r;j++)
+            printf("%d ",a[j][i]);
+        printf("\n");
+    }
+    return 0;
+}
