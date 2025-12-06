cb87355b51886a5cd0f3783c98709f374d29680c
Muksit <muksit09876543@gmail.com>
2025-12-06 13:01:21 +0600

    Create 76.	C Program to Copy All the Elements of One Array to another Array.c

---
+++ 76.	C Program to Copy All the Elements of One Array to another Array.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int n,a[100],b[100],i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    for(i=0;i<n;i++) b[i]=a[i];
+    for(i=0;i<n;i++) printf("%d ",b[i]);
+    return 0;
+}
